c79d6e6da38386610633e101cc9e2c5134aee30c
apiara-tanner <tanner@apiara.net>
2023-05-13 17:37:51 -0700

    done for day

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import {
   PieChartOutlined,
   TeamOutlined,
   UserOutlined,
+  MessageFilled,
+  CloudFilled,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Layout, Menu, Input, Button, Space, Avatar, theme } from 'antd';
-import {UserGrid, ChannelSelect} from './style.js';
+import {UserGrid, ChannelSelect, MenuTab} from './style.js';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -26,9 +28,9 @@ function getItem(label, key, icon, children, type) {
 }
 
 const items: MenuItem[] = [
-  getItem('Option 1', '1', <FileOutlined />),
-  getItem('Option 2', '2', <FileOutlined />),
-  getItem('Files', '9', <FileOutlined />),
+  getItem('Option 1', '1', <CloudFilled />),
+  getItem('Option 2', '2', <CloudFilled />),
+  getItem('Files', '9', <CloudFilled />),
 ];
 
 let userAlias = "Guest";
@@ -41,7 +43,8 @@ const App: React.FC = () => {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider style={{ background: '#1E1E20',
+      <Sider width= '27%' style={{
+        background: '#1E1E20',
         overflow: 'auto',
         height: '100%',
         position: 'fixed',
@@ -49,28 +52,34 @@ const App: React.FC = () => {
         top: 0,
         bottom: 0,
       }}>
-        <img src='/apiara.png' style ={{width: '25%'}}/>
+        <MenuTab>
+          <img src='/apiara.png' style ={{width: '15%'}}/>
+        </MenuTab>
         <Space.Compact style={{ width: '100%', padding: '5%', align: 'center' }}>
-          <Input  placeholder="Server Name" />
+          <Input style = {{background: '#2B2B2D', color: '#88888D', border: 'none'}}/>
           <Button type="primary">Connect</Button>
         </Space.Compact>
 
-        <div>
-        <ChannelSelect> Channel Selection </ChannelSelect>
-        <Menu defaultSelectedKeys={['1']} mode="inline" items={items} style={{ background: '#2B2B2D', color: '#88888D', padding: '5%' }} />
-        </div>
+        <MenuTab>
+        <ChannelSelect> Channel History </ChannelSelect>
+        <Menu defaultSelectedKeys={['1']} mode="inline" items={items} style={{ background: '#2B2B2D', color: '#88888D'}} />
+        </MenuTab>
 
-        <UserGrid>
-          <Avatar size="small" icon={<UserOutlined />} />
-          <div block style={{ margin: '0', background: '#38383A', color: 'white'}}> {userAlias} </div>
-          <Button block style={{ margin: '0', background: '#3A7EFF', color: 'white'}}> Change Alias </Button>
-        </UserGrid>
+          <UserGrid>
+            <Avatar icon={<UserOutlined />} />
+            <Space.Compact style={{ width: '100%', align: 'center' }}>
+              <Input style = {{background: '#38383A', color: '#3A7EFF'}}/>
+              <Button type="primary">Change Alias</Button>
+            </Space.Compact>
+          </UserGrid>
+
+
 
       </Sider>
       <Layout className="site-layout">
         <Header style={{ padding: 0, background: '#1E1E20' }} />
         <Content style={{ margin: '0', background: '#1E1E20' }}>
-          <div style={{ padding: 24, minHeight: 500, background: '#2B2B2D', color: '#88888D' }}>
+          <div style={{ padding: 24, minHeight: 7, background: '#2B2B2D', color: '#88888D', margin: '5%' }}>
             Bill is a cat.
           </div>
 
--- src/style.js
+++ src/style.js
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
-import {Header, Button, Card, Grid, Box, Text, Grommet, Heading, TextArea} from 'grommet';
-import {Typography } from 'antd';
+import {Header, Button, Card, Box, Text, Grommet, Heading, TextArea, Grid} from 'grommet';
+import {Typography} from 'antd';
 
 export const UserGrid = styled(Grid) `
   display: grid;
-  grid-template-columns: 1fr 3fr 3fr;
+  grid-template-columns: 1fr 4fr;
+  background-color: #2B2B2D;
+  padding: 5%;
+  margin: 5%;
+
 `
+
 
 export const ChannelSelect = styled(Typography ) `
   font-size: 15px;
+  font-weight: bold;
   color: #88888D;
+`
 
+export const MenuTab = styled.div`
+  padding: 5%;
+  background-color: #2B2B2D;
+  margin: 5%;
 `
